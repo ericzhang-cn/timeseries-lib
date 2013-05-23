5824284c32efc5ab44f4cb70030c505aae0da469
ericzhang <ericzhang.buaa@gmail.com>
2013-05-23 18:26:40 +0800

    add Statistics

---
+++ src/main/java/org/codinglabs/timeseries_lib/lib/Statistics.java
@@ -0,0 +1,41 @@
+package org.codinglabs.timeseries_lib.lib;
+
+import java.util.ArrayList;
+
+/**
+ * Statistical functions
+ * 
+ * @author ericzhang
+ */
+public class Statistics {
+	/***
+	 * Calculate autocorrelation
+	 * 
+	 * @param series
+	 *            Time series
+	 * @param k
+	 *            Order
+	 * @return Autocorrelation coefficient
+	 */
+	public static double autocorrelation(ArrayList<Double> series, int k) {
+		if (k < 1 || series.size() <= k) {
+			return 0D;
+		}
+
+		double sum = 0D;
+		for (Double s : series) {
+			sum += s;
+		}
+		double avg = sum / series.size();
+
+		double a = 0D, b = 0D;
+		for (int i = 0; i < series.size(); i++) {
+			b += (series.get(i) - avg) * (series.get(i) - avg);
+			if (i >= k) {
+				a += (series.get(i) - avg) * (series.get(i - k) - avg);
+			}
+		}
+
+		return a / b;
+	}
+}
---
+++ src/test/java/org/codinglabs/timeseries_lib/lib/StatisticsTest.java
@@ -0,0 +1,30 @@
+package org.codinglabs.timeseries_lib.lib;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class StatisticsTest extends TestCase {
+	public void testAutocorrelation() {
+		ArrayList<Double> series = new ArrayList<Double>();
+		series.add(1D);
+		series.add(2D);
+		series.add(3D);
+		series.add(4D);
+		series.add(5D);
+		series.add(6D);
+		series.add(7D);
+		series.add(8D);
+		series.add(9D);
+		series.add(10D);
+
+		DecimalFormat df = new DecimalFormat("0.000");
+
+		Assert.assertEquals(df.format(0.700),
+				df.format(Statistics.autocorrelation(series, 1)));
+		Assert.assertEquals(df.format(0.412),
+				df.format(Statistics.autocorrelation(series, 2)));
+	}
+}
