e3a9e3c7e5c47076d17a13a5338a28232e42416b
ericzhang <ericzhang.buaa@gmail.com>
2013-05-24 18:18:49 +0800

    re

--- src/test/java/org/codinglabs/timeseries_lib/arima/StatisticsTest.java
+++ src/test/java/org/codinglabs/timeseries_lib/arima/StatisticsTest.java
@@ -9,71 +9,49 @@ import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class StatisticsTest extends TestCase {
+    private ArrayList<Double> series;
+    private DecimalFormat df;
+    private Statistics st;
+
+    protected void setUp() {
+        series = new ArrayList<Double>();
+        series.add(1D);
+        series.add(2D);
+        series.add(3D);
+        series.add(4D);
+        series.add(5D);
+        series.add(6D);
+        series.add(7D);
+        series.add(8D);
+        series.add(9D);
+        series.add(10D);
+
+        df = new DecimalFormat("0.000");
+        st = Statistics.getInstance();
+    }
+
     public void testAutocorrelation() {
-	ArrayList<Double> series = new ArrayList<Double>();
-	series.add(1D);
-	series.add(2D);
-	series.add(3D);
-	series.add(4D);
-	series.add(5D);
-	series.add(6D);
-	series.add(7D);
-	series.add(8D);
-	series.add(9D);
-	series.add(10D);
-
-	DecimalFormat df = new DecimalFormat("0.000");
-	Statistics st = Statistics.getInstance();
-
-	Assert.assertEquals(df.format(1.000),
-		df.format(st.autocorrelation(series, 0)));
-	Assert.assertEquals(df.format(0.700),
-		df.format(st.autocorrelation(series, 1)));
-	Assert.assertEquals(df.format(0.412),
-		df.format(st.autocorrelation(series, 2)));
+        Assert.assertEquals(df.format(1.000),
+                df.format(st.autocorrelation(series, 0)));
+        Assert.assertEquals(df.format(0.700),
+                df.format(st.autocorrelation(series, 1)));
+        Assert.assertEquals(df.format(0.412),
+                df.format(st.autocorrelation(series, 2)));
     }
 
     public void testAcf() {
-	ArrayList<Double> series = new ArrayList<Double>();
-	series.add(1D);
-	series.add(2D);
-	series.add(3D);
-	series.add(4D);
-	series.add(5D);
-	series.add(6D);
-	series.add(7D);
-	series.add(8D);
-	series.add(9D);
-	series.add(10D);
+        ArrayList<Double> acf = st.acf(series, 9);
 
-	DecimalFormat df = new DecimalFormat("0.000");
-	Statistics st = Statistics.getInstance();
-	ArrayList<Double> acf = st.acf(series, 9);
-
-	Assert.assertEquals(df.format(1.000), df.format(acf.get(0)));
-	Assert.assertEquals(df.format(0.700), df.format(acf.get(1)));
-	Assert.assertEquals(df.format(0.412), df.format(acf.get(2)));
+        Assert.assertEquals(df.format(1.000), df.format(acf.get(0)));
+        Assert.assertEquals(df.format(0.700), df.format(acf.get(1)));
+        Assert.assertEquals(df.format(0.412), df.format(acf.get(2)));
     }
 
     public void testPacf() {
-	ArrayList<Double> series = new ArrayList<Double>();
-	series.add(1D);
-	series.add(2D);
-	series.add(3D);
-	series.add(4D);
-	series.add(5D);
-	series.add(6D);
-	series.add(7D);
-	series.add(8D);
-	series.add(9D);
-	series.add(10D);
+        ArrayList<Double> pacf = st.pacf(series, 9);
 
-	DecimalFormat df = new DecimalFormat("0.000");
-	Statistics st = Statistics.getInstance();
-	ArrayList<Double> pacf = st.pacf(series, 9);
-
-	Assert.assertEquals(df.format(0.700), df.format(pacf.get(1)));
-	Assert.assertEquals(df.format(-0.153), df.format(pacf.get(2)));
-	Assert.assertEquals(df.format(-0.155), df.format(pacf.get(3)));
+        Assert.assertEquals(df.format(0.700), df.format(pacf.get(1)));
+        Assert.assertEquals(df.format(-0.153), df.format(pacf.get(2)));
+        Assert.assertEquals(df.format(-0.155), df.format(pacf.get(3)));
     }
 }
--- src/test/java/org/codinglabs/timeseries_lib/arima/TransformationTest.java
+++ src/test/java/org/codinglabs/timeseries_lib/arima/TransformationTest.java
@@ -2,36 +2,48 @@ package org.codinglabs.timeseries_lib.arima;
 
 import java.util.ArrayList;
 
-import org.codinglabs.timeseries_lib.arima.Transformation;
-
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class TransformationTest extends TestCase {
+    private ArrayList<Double> series;
+    private Transformation tf;
+
+    protected void setUp() {
+        series = new ArrayList<Double>();
+        series.add(1D);
+        series.add(2D);
+        series.add(3D);
+        series.add(4D);
+        series.add(5D);
+        series.add(6D);
+        series.add(7D);
+        series.add(8D);
+        series.add(9D);
+        series.add(10D);
+
+        tf = Transformation.getInstance();
+    }
+
     public void testDifference() {
-	ArrayList<Double> series = new ArrayList<Double>();
-	series.add(1D);
-	series.add(2D);
-	series.add(3D);
-	series.add(4D);
-	series.add(5D);
-	series.add(6D);
-	series.add(7D);
-	series.add(8D);
-	series.add(9D);
-	series.add(10D);
+        ArrayList<Double> df0 = tf.diff(series, 0);
+        ArrayList<Double> df1 = tf.diff(series, 1);
+        ArrayList<Double> df2 = tf.diff(series, 2);
 
-	Transformation tf = Transformation.getInstance();
-	ArrayList<Double> df0 = tf.diff(series, 0);
-	ArrayList<Double> df1 = tf.diff(series, 1);
-	ArrayList<Double> df2 = tf.diff(series, 2);
+        Assert.assertEquals(1D, df0.get(0));
+        Assert.assertEquals(1D, df1.get(0));
+        Assert.assertEquals(0D, df2.get(0));
 
-	Assert.assertEquals(1D, df0.get(0));
-	Assert.assertEquals(1D, df1.get(0));
-	Assert.assertEquals(0D, df2.get(0));
+        Assert.assertEquals(10D, df0.get(9));
+        Assert.assertEquals(1D, df1.get(8));
+        Assert.assertEquals(0D, df2.get(7));
+    }
 
-	Assert.assertEquals(10D, df0.get(9));
-	Assert.assertEquals(1D, df1.get(8));
-	Assert.assertEquals(0D, df2.get(7));
+    public void testLog() {
+        ArrayList<Double> log = tf.log(series);
+        
+        Assert.assertEquals(Math.log(1D), log.get(0));
+        Assert.assertEquals(Math.log(2D), log.get(1));
+        Assert.assertEquals(Math.log(3D), log.get(2));
     }
 }
